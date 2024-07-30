bd51241cfa751b2c3e588c9a32cdc46be6815c24
karinehabhenry <161515319+karinehabhenry@users.noreply.github.com>
2024-07-30 14:05:41 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from "../modules/head/head.component";
+import { BodyComponent } from "../modules/body/body.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeadComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
