023489f0086a86fe7890627f955040142d81243c
karinehabhenry <161515319+karinehabhenry@users.noreply.github.com>
2024-07-30 20:10:16 +0300

    loop

--- src/modules/body/body.component.ts
+++ src/modules/body/body.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-
+  List = [
+    { label: 'Name', value: 'Karin Ehab' },
+    { label: 'Academic Year', value: 'Year 3' },
+    { label: 'Goals', value: 'My own Startup' },
+    { label: 'Hobbies', value: 'Squash' },
+    { label: 'Technologies', value: 'Microsoft' }
+  ];
 }
+
